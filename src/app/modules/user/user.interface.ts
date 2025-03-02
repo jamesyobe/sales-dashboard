99c38e7494507c0f2abbce98f23f8a41fad43d9d
jamesyobe <jamesyobe@yahoo.com>
2025-03-01 22:05:34 -0600

    Auto-generated commit

--- src/app/modules/user/user.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    firstName?: string,
-    lastName?: string,
-    username?: string,
-    email?: string,
-    confirmEmail?: string,
-    password?: string,
-    confirmPassword?: string
-}
